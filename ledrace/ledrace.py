7ef9b653af6ab1ced99a0267f1df7b3c650c8f3a
Georg Heyden <archibaldschlick@gmx.de>
2015-09-28 00:30:44 +0200

    cleaning up the code

--- ledrace/ledrace.py
+++ ledrace/ledrace.py
@@ -1,28 +1,35 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
-GPIO.setup(17, GPIO.OUT)
+def setup():
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(4, GPIO.OUT)
+	GPIO.setup(17, GPIO.OUT)
 
+def loop():
+	count = 3
+	print count
+	while count > 0:
+  	 	GPIO.output(17,False)
+		time.sleep(1)
+    	GPIO.output(17,True)
+    	time.sleep(1)
+    	count = count -1
+    	print count
+	#else:
+    	print "GO!"
+    	GPIO.output(4,False)
+    	time.sleep(3)
+    	GPIO.output(4,True)
+def destroy():
+	GPIO.output(4,False)
+	GPIO.output(17,True)
+	GPIO.cleanup()
 
-
-count = 3
-print count
-
-# endless loop, on/off for 1 second
-while count > 0:
-    GPIO.output(17,False)
-    time.sleep(1)
-    GPIO.output(17,True)
-    time.sleep(1)
-    count = count -1
-    print count
-
-else:
-    print "GO!"
-    GPIO.output(4,False)
-    time.sleep(3)
-    GPIO.output(4,True)
-    
+if __name__ == '__main__':     # Program start from here
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+        destroy()
