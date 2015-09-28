bb6a3fbf8008947a7f54addc2735d98c53313f67
Georg Heyden <archibaldschlick@gmx.de>
2015-09-28 13:50:10 +0200

    neuer Versuch_muss noch getestet werden

---
+++ led_button_red_green/led_button_red_green.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+buttonPin = 13
+ledGreenPin = 11
+ledRedPin = 12
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(ledGreenPin, GPIO.OUT)
+	GPIO.setup(ledRedPin, GPIO.OUT)
+	GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.output(ledGreenPin, GPIO.HIGH)
+	GPIO.output(ledRedPin, GPIO.HIGH)
+	
+def loop():
+	while True: 
+		if GPIO.input(buttonPin) == GPIO.LOW:
+		   print '...red led on'
+		   GPIO.output(ledRedPin, GPIO.LOW)
+		   GPIO.output(ledGreenPin, GPIO.HIGH)
+	else: 
+		print 'green led on...'
+		GPIO.output(ledGreenPin, GPIO.LOW)
+		GPIO.output(ledRedPin, GPIO.HIGH)
+
+def destroy():
+	GPIO.output(ledGreenPin, GPIO.HIGH)
+	GPIO.output(ledRedPin, GPIO.HIGH)
+	GPIO.cleanup()
+
+if __name__ == '__main__':    
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
