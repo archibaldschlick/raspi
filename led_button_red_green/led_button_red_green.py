facabb5236d98bd3ba350c691a2338a6c5bd662a
archibaldschlick <archibaldschlick@gmx.de>
2015-09-28 22:02:00 +0200

    code läuft

--- led_button_red_green/led_button_red_green.py
+++ led_button_red_green/led_button_red_green.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
-import time
 
 buttonPin = 13
 ledGreenPin = 11
@@ -13,17 +12,16 @@ def setup():
 	GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.output(ledGreenPin, GPIO.HIGH)
 	GPIO.output(ledRedPin, GPIO.HIGH)
-	
 def loop():
 	while True: 
 		if GPIO.input(buttonPin) == GPIO.LOW:
 		   print '...red led on'
 		   GPIO.output(ledRedPin, GPIO.LOW)
 		   GPIO.output(ledGreenPin, GPIO.HIGH)
-	else: 
-		print 'green led on...'
-		GPIO.output(ledGreenPin, GPIO.LOW)
-		GPIO.output(ledRedPin, GPIO.HIGH)
+		else: 
+			print 'green led on...'
+			GPIO.output(ledGreenPin, GPIO.LOW)
+			GPIO.output(ledRedPin, GPIO.HIGH)
 
 def destroy():
 	GPIO.output(ledGreenPin, GPIO.HIGH)
